fbe6b6a17ef153058e699c5b8b14c523b15d8e51
ruijie.lou <ruijie.lou@163.com>
2020-11-19 17:11:42 +0800

    修改添加接口调试

--- app.js
+++ app.js
@@ -1,38 +1,99 @@
 //app.js
 App({
+  showSettingToast: function (e) {
+    // debugger
+    // wx.showModal({
+    //   title: '提示！',
+    //   confirmText: '去设置',
+    //   showCancel: false,
+    //   content: "是否授权",
+    //   success: function (res) {
+    //     if (res.confirm) {
+    //       wx.navigateTo({
+    //         url: '../setting/setting',
+    //       })
+    //     }
+    //   }
+    // })
+  },
+  login: function () {
+    wx.login({
+      success: (res) => {
+        this.globalData.code = res.code;
+        this.getConfig();
+        wx.navigateTo({
+          url: "../index/index",
+        });
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      },
+    });
+  },
   onLaunch: function () {
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var _this = this;
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+
+    // var accountInfo = wx.getAccountInfoSync();
+    // a2ecb26b08bd03411a132ff1ec08b981
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+
+    //检查登录态是否过期。
+    wx.checkSession({
+      success() {
+        //session_key 未过期，并且在本生命周期一直有效
+        _this.getConfig();
+      },
+      fail() {
+        // session_key 已经失效，需要重新执行登录流程
+        // wx.login().then((res) => console.log(res, "====login")); //重新登录
+        _this.login();
+      },
+    });
+
     // 获取用户信息
+    /**
+     * 1. 先获取用户是否授权，不授权就用临时code去使用
+     * 2. 用户授权后，获取用户信息，并且拿到用户的openid
+     * 3. 请求历史记录
+     * 4. 正常渲染页面
+     */
+  },
+  getConfig() {
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
+      success: (res) => {
+        // res.authSetting["scope.userInfo"] = false;
+        if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
-            success: res => {
+            success: (res) => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(res);
               }
-            }
-          })
+            },
+          });
+          wx.navigateTo({
+            url: "../index/index",
+          });
+        } else {
+          wx.navigateTo({
+            url: "../login/login",
+          });
         }
-      }
-    })
+      },
+    });
   },
+  onShow: function () {},
   globalData: {
-    userInfo: null
-  }
-})
+    userInfo: null,
+    appId: "",
+    code: "",
+    canIUse: wx.canIUse("button.open-type.getUserInfo"),
+  },
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,149 +1,204 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+const app = getApp();
+console.log(app, "=====app");
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    image: '/assets/placeholder.jpg',
-    logImage: '/assets/syslogs.svg',
-    showTips: false,
-    result: null
+    image: "",
+    logImage: "/assets/syslogs.svg",
+    result: "",
+    allScore: 0,
+    loading: false
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: "../logs/logs",
+    });
+  },
+  clearData() {
+    const that = this
+    console.log(that);
+    debugger
+    if(that.data.loading) return
+    that.setData({
+      result: "",
+      image: ""
     })
   },
-   /**
+  getOpenId:function() {
+    wx.request({
+      url:
+        "http://192.168.0.130:8080/auth/code2Session?code=" +
+        app.globalData.code, //仅为示例，并非真实的接口地址
+
+      header: {
+        "content-type": "application/json", // 默认值
+      },
+      success(res) {
+        if (res.data.code == 200) {
+          wx.setStorageSync("openId", res.data.data);
+          this.getTotal();
+        } else {
+          wx.showToast({
+            title: "获取用户id失败，检查服务器连接状态",
+            icon: "warning",
+            duration: 2000,
+          });
+        }
+      },
+    });
+  },
+  /**
    * 分析照片
    */
-  detectImage (src) {
-    const that = this
-
+  detectImage(src) {
+    const that = this;
     // 取消之前的结果显示
-    that.setData({ result: null })
-
+    that.setData({ result: null });
     // loading
-    wx.showLoading({ title: '分析中...' })
-
-    // wx.request({
-    //   url: 'test.php', //仅为示例，并非真实的接口地址
-    //   data: {
-    //     x: '',
-    //     y: ''
-    //   },
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success (res) {
-    //     console.log(res.data)
-    //   }
-    // })
-    
+    this.setData({loading: true})
+    wx.showLoading({ title: "分析中..." });
+    // multipartFile
     // 将图片上传至 AI 服务端点
-    wx.uploadFile({
-      url: 'https://ai.qq.com/cgi-bin/appdemo_detectface',
-      name: 'image_file',
-      filePath: src,
-      success (res) {
-        // 解析 JSON
-        const result = JSON.parse(res.data)
-
-        if (result.ret === 0) {
-          // 成功获取分析结果
-          that.setData({ result: result.data.face[0] })
-        } else {
-          // 检测失败
-          wx.showToast({ icon: 'none', title: '找不到你的小脸蛋' })
-        }
-        // end loading
-        wx.hideLoading()
-      }
-    })
+    try {
+      wx.uploadFile({
+        url:
+          "http://192.168.0.130:8080/ticket/createItem?wxOpenId=" +
+          app.globalData.openId,
+        filePath: src,
+        header: {
+          "Content-Type": "multipart/form-data",
+        },
+        name: "multipartFile",
+        success(res) {
+          
+          if (res.data) {
+            console.log('解析图片成功');
+            // 成功获取分析结果
+            const result = JSON.parse(res.data)
+            if (result.data && result.code == 200) {
+              that.setData({ result: result.data });
+            } else {
+              // 检测失败
+              wx.showToast({ icon: "none", title: "您上传的是无效图片" });
+            }
+          } else {
+            // 检测失败
+            wx.showToast({ icon: "none", title: "解析图片失败" });
+          }
+          // end loading
+          wx.hideLoading();
+          that.setData({loading: false})
+        },
+      });
+    } catch (error) {
+      wx.showToast({ icon: "none", title: error });
+    }
   },
 
   /**
    * 获取照片
    */
-  getImage () {
-    const that = this
+  getImage() {
+    const that = this;
 
     // 调用系统 API 选择或拍摄照片
     wx.chooseImage({
       sourceType: ["camera", "album"], // camera | album
-      sizeType: ['compressed'], // original | compressed
+      sizeType: ["compressed"], // original | compressed
       count: 1,
-      success (res) {
+      success(res) {
         // 取照片对象
-        const image = res.tempFiles[0]
+        console.log(res, "tup");
+        const image = res.tempFiles[0];
 
         // 图片过大
         if (image.size > 1024 * 1000) {
-          return wx.showToast({ icon: 'none', title: '图片过大, 请重新拍张小的！' })
+          return wx.showToast({
+            icon: "none",
+            title: "图片过大, 请重新拍张小的！",
+          });
         }
 
         // 显示到界面上
-        that.setData({ image: image.path })
+        that.setData({ image: image.path });
 
         // 分析检测人脸
-        that.detectImage(image.path)
-      }
-    })
-
-    // 关闭 Tips 显示
-    this.setData({ showTips: false })
+        that.detectImage(image.path);
+      },
+    });
   },
   onLoad: function () {
-    const isUsed = wx.getStorageSync('is_used')
-    if (isUsed) return
-    // 第一次使用显示 Tips
-    this.setData({ showTips: true })
+    const openId = wx.getStorageSync("openId");
+    if (openId) {
+      app.globalData.openId = openId;
+      this.getTotal();
+    } else {
+      this.getOpenId();
+    }
+    const isUsed = wx.getStorageSync("is_used");
+    if (isUsed) return;
     // 并记住用使用过了
-    wx.setStorageSync('is_used', true)
+    wx.setStorageSync("is_used", true);
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
+        hasUserInfo: true,
+      });
+    } else if (app.globalData.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+      app.userInfoReadyCallback = (res) => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
+          hasUserInfo: true,
+        });
+      };
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+        success: (res) => {
+          app.globalData.userInfo = res.userInfo;
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+            hasUserInfo: true,
+          });
+        },
+      });
     }
   },
-  /**右上角点击分享 */
-  onShareAppMessage () {
-    if (!this.data.result) return
-    // 如果有分析结果，则分享
-    return { title: `刚刚测了我的颜值「${this.data.result.beauty}」你也赶紧来试试吧！` }
+  getTotal() {
+    // /ticket/getSumIntegral
+    wx.request({
+      url:
+        "http://192.168.0.130:8080/ticket/getSumIntegral?wxOpenId=" +
+        app.globalData.openId,
+      method: "POST",
+      header: {
+        "content-type": "application/json", // 默认值
+      },
+      success(res) {
+        if (res.data.code == 200) {
+          console.log(res.data);
+        } else {
+          wx.showToast({
+            title: "获取总积分失败，检查服务器连接状态",
+            icon: "warning",
+            duration: 2000,
+          });
+        }
+      },
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+  /**右上角点击分享 */
+  onShareAppMessage() {
+    if (!this.data.result) return;
+    // 如果有分析结果，则分享
+    return {
+      title: `刚刚测了分析了我的小票「${this.data.result.beauty}」你也赶紧来试试吧！`,
+    };
+  },
+});
